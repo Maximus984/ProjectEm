36e6c6d172020980d0777ce1694cee79eb9d7989
Maximus984 <maximus.thurston@hgs.hiddengeniusproject.org>
2026-03-03 18:15:24 -0800

    Bump service worker cache version for fresh assets

--- apps/web/public/sw.js
+++ apps/web/public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "projectm-shell-v3";
+const CACHE_NAME = "projectm-shell-v4";
 const scopePath = new URL(self.registration.scope).pathname.replace(/\/$/, "");
 
 const withScope = (path) => `${scopePath}${path}`;
